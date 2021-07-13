75e88d689d2c54c09eb3a146f9ad0d9e8ea2faac
ToQuyenPhan <87388349+ToQuyenPhan@users.noreply.github.com>
2021-07-14 02:02:52 +0700

    Update Program 1 - Paying For Electricity.c

--- Program 1 - Paying For Electricity.c
+++ Program 1 - Paying For Electricity.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	//Khai bao bien
 	int x;//bien x duoc su dung cho vong lap (khong lien quan den yeu cau bai tap)
